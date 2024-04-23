972338d3bd6156029c37c9483ad7a3b7ee025e06
Will Hack <18036720+willhack@users.noreply.github.com>
2024-04-23 12:40:08 -0400

    move thumb clusters closer to miryoku

--- keyboards/draculad/keymaps/gotham/keymap.c
+++ keyboards/draculad/keymaps/gotham/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LCTL_T(KC_A),  LALT_T(KC_S),  LGUI_T(KC_D),  LSFT_T(KC_F),  KC_G,                    KC_H,  RSFT_T(KC_J),  RGUI_T(KC_K),   LALT_T(KC_L),  RCTL_T(KC_SCLN),
         KC_Z, KC_X,    KC_C,    KC_V,    KC_B,                                                 KC_N,             KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
                                           KC_MUTE,                                              TG(_ADJ),
-                                  XXXXXXX, KC_BSPC, LT(_MUS,KC_SPC),    LT(_NUM,KC_DEL), LT(_SYMB,KC_ENT), KC_CAPS
+                                  LT(_MUS, KC_ESC), LT(_NUM, KC_SPC), LT(XXXXXXX, KC_TAB),    LT(_SYMB, KC_ENT), LT(_NUM, KC_BSPC), LT(XXXXXXX, KC_DEL)
     ),
     [_NUM] = LAYOUT(
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
